28cb87fa74ee96593a9b317c0e096e3bd829e134
Cristian <cris.ferreirak10@gmail.com>
2023-05-31 09:52:52 -0700

    buscas

---
+++ Api_Almoxarifado_Mirvi/Controllers/BuscasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api_Almoxarifado_Mirvi.Controllers
+{
+    public class BuscasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
---
+++ Api_Almoxarifado_Mirvi/Services/BuscasService.cs
@@ -0,0 +1,35 @@
+﻿using Api_Almoxarifado_Mirvi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Almoxarifado_Mirvi.Services
+{
+    public class BuscasService
+    {
+
+        private readonly Api_Almoxarifado_MirviContext _context;
+
+        public BuscasService(Api_Almoxarifado_MirviContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+    }
+}
